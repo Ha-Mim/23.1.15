12c6f743f0a91f78024a6708b0278dcb4c6c287a
Ha-Mim <hamim.hyder@gmail.com>
2015-01-24 08:14:35 +0600

    Added

---
+++ DynamicPage/DynamicPage/index.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="DynamicPage.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <input type= "text" name="employeeName" id="employeeName" value="<%= GetName()%>"/>
+    <br/>
+    <input type="text" name="employeeAddress" id="employeeAddress" value="<%= GetAddress()%>"/>
+</body>
+</html>
---
+++ DynamicPage/DynamicPage/index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DynamicPage
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string GetName()
+        {
+            return "Ha-Mim";
+        }
+
+        public string GetAddress()
+        {
+            return "Dhaka";
+        }
+    }
+}
